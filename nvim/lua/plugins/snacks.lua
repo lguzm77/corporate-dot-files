f305147851d6b24873d82baa800e453edb78285f
lguzm77 <lguzmm77@icloud.com>
2025-12-25 09:53:54 -0800

    Disable snacks.explorer keybindings

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -1,5 +1,11 @@
 return {
   "folke/snacks.nvim",
+  keys = {
+    { "<leader>fe", false },
+    { "<leader>fE", false },
+    { "<leader>E", false },
+    { "<leader>e", false },
+  },
   opts = {
     explorer = { enabled = false },
     terminal = {
