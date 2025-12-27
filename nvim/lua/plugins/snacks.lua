cee47fa6f1e9cb5b9e8466d0a8f5324c4410c2cc
lguzm77 <lguzmm77@icloud.com>
2025-12-26 16:07:32 -0800

    Tweak snacks.terminal

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -9,10 +9,13 @@ return {
   opts = {
     explorer = { enabled = false },
     terminal = {
+      enabled = true,
+      interactive = true,
       win = {
         style = "float",
-        width = math.floor(vim.o.columns * 90),
-        height = math.floor(vim.o.columns * 90),
+        width = math.floor(vim.o.columns * 0.95),
+        height = math.floor(vim.o.columns * 0.95),
+        border = "rounded",
       },
     },
   },
